cc1018d2154f0ed9d19e2dbb6c1a09d2f22eb131
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-08-30 13:51:34 +0200

    successfully fetched google spreadsheet

--- draft.py
+++ draft.py
@@ -2,9 +2,10 @@ from io import BytesIO
 import pandas as pd
 import requests
 
-r = requests.get('https://docs.google.com/spreadsheets/d/1QCnD7HpnaAkYqb4f70uqqmOtZL_IYoAuRRe8AeF6stI/edit#gid=0&output=csv')
+r = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vSus1t39Sy6KKMDn3gaBwpcykMhTIUIEf0vLiAjTqv_IZb0uAyoIo_C6ncm73TR9hk6TZyDrvawqwRH/pub?gid=0&single=true&output=csv')
 data = r.content
 
-df = pd.read_csv(BytesIO(data), names=['ConferenceName', 'Acronym', 'CORE2023', 'Year', 'AnonymityStarts', 'PaperDue', 'NotifOfAcceptance', 'ConfStarts', 'ConfEnds'], error_bad_lines=False)
+df = pd.read_csv(BytesIO(data), names=['ConferenceName', 'Acronym', 'CORE2023', 'Year', 'AnonymityStarts', 'PaperDue', 'NotifOfAcceptance', 'ConfStarts', 'ConfEnds'], header=0)
                 #  parse_dates=['Paper due']
-print(df.head())
+
+print(df)
